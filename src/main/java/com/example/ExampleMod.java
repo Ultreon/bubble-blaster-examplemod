defa0f3da8fe94942b4640422b51ba2c24af0016
xypercode <qboiwastaken@gmail.com>
2023-09-28 23:23:49 +0200

    Add events

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -2,6 +2,8 @@ package com.example;
 
 import com.example.init.ModBubbles;
 import com.ultreon.bubbles.BubbleBlaster;
+import com.ultreon.bubbles.event.v1.GameEvents;
+import com.ultreon.bubbles.event.v1.WindowEvents;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,9 +19,12 @@ public class ExampleMod implements ModInitializer {
     @Override
     public void onInitialize() {
         ModBubbles.register();
+
+        GameEvents.CLIENT_STARTED.listen(game -> LOGGER.info("Hello world from 'CLIENT_STARTED' event!"));
+        GameEvents.CLIENT_STARTED.listen(ExampleMod::onClientStarted);
     }
 
-    private void started(BubbleBlaster game) {
-
+    private static void onClientStarted(BubbleBlaster game) {
+        LOGGER.info("Hello world from onClientStarted method!");
     }
 }
